a1f75e92b011bd4132831d3d0ffcd39e342e3b42
SabinaAzizova <tu7USHCNJ.code.edu.az>
2024-03-27 22:45:19 +0400

    task5 1

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student ("Sabina","Azizova",19,122,0);
+            student.ShowInfo();
+        }
+    }
+}
---
+++ ConsoleApp5/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp5
+{
+    internal class Student : Person
+        {
+  private string name; // field
+
+  public string Name   // property
+  {
+    get { return name; }   // get method
+    set { name = value; }  // set method
+  }
+    
+        public byte StudentId;
+        public double Point;
+        public Student(string name, string surname, byte age, byte StudentId, double Point) : base(name, surname, age)
+        {
+            StudentId = StudentId;
+            Point = Point;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} {Surname} {Age}");
+        }  
+    }
+}
